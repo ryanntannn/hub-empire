3a53d358ba76d2806c6f6f387b3d6165316efe90
ryanntannn <tanryan777@gmail.com>
2022-01-24 11:50:48 +0800

    add frontend types and api

---
+++ client/src/types/api.ts
@@ -0,0 +1,59 @@
+import { TradeOffer, UserData, UserDataBasic, UserDataMin } from './types';
+
+// GET /home
+export interface GetHomeDataRes {
+	myData: UserDataBasic;
+}
+
+//GET /my-cards
+export interface GetMyCardsRes {
+	cards: number[];
+}
+
+//GET /leaderboard
+export interface GetLeaderboardRes {
+	users: UserDataBasic[];
+}
+
+//GET /users-min
+export interface GetUsersMinRes {
+	users: UserDataMin[];
+}
+
+//GET /user
+export interface GetUserDataRes {
+	user: UserData;
+}
+
+//POST /use-card
+export interface PostUseCardParams {
+	cardId: number;
+	targetId?: number;
+	targetCardId?: number;
+	selfCardId?: number;
+}
+
+//POST /send-trade
+export interface PostSendTradeParams {
+	idTo: number;
+	cardIdsFrom: number[];
+	cashFrom: number;
+	cardIdsTo: number[];
+	cashTo: number;
+}
+
+//GET /trade/inbox & /trade/history
+export interface GetTradeInboxParams {
+	showAmount: number; // e.g. show 5 in one page
+	page: number; //page number
+}
+export interface GetTradeInboxRes {
+	trades: TradeOffer[];
+	pages: number; //total number of pages
+}
+
+//POST /trade/action
+export interface PostAcceptTradeParams {
+	id: number;
+	accept: boolean;
+}
