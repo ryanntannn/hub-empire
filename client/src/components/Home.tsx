dcce06e510ededb5a8cfdd1581e64aa26bee9a01
ryanntannn <tanryan777@gmail.com>
2022-02-04 14:31:56 +0800

    add authenticatedGet

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,14 +1,29 @@
 import { Link } from 'react-router-dom';
-import { Button, Col, Container, Row } from 'reactstrap';
+import { Button, Col, Container, Row, Spinner } from 'reactstrap';
 import NavButton from './NavButton';
 import AuthenticationContext from '../contexts/AuthenticationContext';
-import React from 'react';
+import React, { useEffect } from 'react';
 import useAuth from '../contexts/AuthenticationContext';
 import { useNavigate } from 'react-router-dom';
+import { GetHomeDataRes } from '../types/api';
+import { autheticatedGet } from '../utils/AxiosBase';
 
 function Home() {
 	let navigate = useNavigate();
 	const auth = useAuth();
+
+	const [homeData, setHomeData] = React.useState<GetHomeDataRes | null>(null);
+
+	React.useState(() => {
+		if (homeData != null) return;
+		autheticatedGet('/home')
+			.then((res: any) => {
+				setHomeData(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
 
 	// React.useEffect(() => {
 	// 	if (auth?.user.accessToken == '') {
@@ -18,66 +33,70 @@ function Home() {
 
 	return (
 		<div className='home page'>
-			<Container className='mt-5'>
-				<h1 className='title'>🏠 Hub Empire</h1>
-				<Row className='mb-3'>
-					<Col>
-						<div className='rounded-box shadow'>
-							<h2 className='no-padding huge-and-bold'>
-								{auth.user.userData.displayName}
-							</h2>
-							<p className='no-padding'>net-worth:</p>
-							<h2 className='no-padding huge-and-bold'>
-								${auth.user.userData.netWorth}
-							</h2>
-							<p className='no-padding'>earnings per day:</p>
-							<h2
-								className='no-padding big-and-bold'
-								style={{ color: 'green' }}>
-								${auth.user.userData.netEarnings}
-							</h2>
-						</div>
-					</Col>
-				</Row>
-				<Row className='mb-3 gx-3'>
-					<Col>
-						<NavButton
-							title='My Cards'
-							emoji='🏢'
-							linkTo='/my-cards'
-							color='#6F42C1'
-						/>
-					</Col>
+			{homeData != null ? (
+				<Container className='mt-5'>
+					<h1 className='title'>🏠 Hub Empire</h1>
+					<Row className='mb-3'>
+						<Col>
+							<div className='rounded-box shadow'>
+								<h2 className='no-padding huge-and-bold'>
+									{homeData.myData.displayName}
+								</h2>
+								<p className='no-padding'>net-worth:</p>
+								<h2 className='no-padding huge-and-bold'>
+									${homeData.myData.netWorth}
+								</h2>
+								<p className='no-padding'>earnings per day:</p>
+								<h2
+									className='no-padding big-and-bold'
+									style={{ color: 'green' }}>
+									${homeData.myData.netEarnings}
+								</h2>
+							</div>
+						</Col>
+					</Row>
+					<Row className='mb-3 gx-3'>
+						<Col>
+							<NavButton
+								title='My Cards'
+								emoji='🏢'
+								linkTo='/my-cards'
+								color='#6F42C1'
+							/>
+						</Col>
 
-					<Col>
-						<NavButton
-							title='Trade'
-							emoji='🤝‍'
-							linkTo='/trade'
-							color='#007BFF'
-						/>
-					</Col>
-				</Row>
-				<Row className='mb-3 gx-3'>
-					<Col>
-						<NavButton
-							title='Leaderboard'
-							emoji='🏆'
-							linkTo='/leaderboard'
-							color='#28A745'
-						/>
-					</Col>
+						<Col>
+							<NavButton
+								title='Trade'
+								emoji='🤝‍'
+								linkTo='/trade'
+								color='#007BFF'
+							/>
+						</Col>
+					</Row>
+					<Row className='mb-3 gx-3'>
+						<Col>
+							<NavButton
+								title='Leaderboard'
+								emoji='🏆'
+								linkTo='/leaderboard'
+								color='#28A745'
+							/>
+						</Col>
 
-					<Col>
-						<NavButton
-							title='My Profile'
-							emoji='👤'
-							linkTo='/profile'
-							color='#EBB30B'
-						/>
-					</Col>
-				</Row>
-			</Container>
+						<Col>
+							<NavButton
+								title='My Profile'
+								emoji='👤'
+								linkTo='/profile'
+								color='#EBB30B'
+							/>
+						</Col>
+					</Row>
+				</Container>
+			) : (
+				<Spinner />
+			)}
 		</div>
 	);
 }
--- client/src/utils/AxiosBase.ts
+++ client/src/utils/AxiosBase.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
+import cookies from './Cookies';
 
 //const Endpoint = 'http://localhost:42069';
 //const Endpoint = 'https://tonvo.net/';
@@ -7,7 +8,16 @@ const Endpoint: string =
 const AxiosBase: AxiosInstance = axios.create({
 	baseURL: Endpoint,
 });
+const token = cookies.get('dnjt');
+
+const configHeader = {
+	headers: { Authorization: `Bearer ${token}` },
+};
 
 export default AxiosBase;
+
+export function autheticatedGet<T>(url: string): Promise<T> {
+	return AxiosBase.get(url, configHeader);
+}
 
 export { AxiosBase, Endpoint };
