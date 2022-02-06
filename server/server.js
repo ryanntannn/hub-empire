56536a3cb0058a185a790c10a5a574b688ef29c6
Chivess <kokbochang@gmail.com>
2022-02-06 13:08:50 +0800

    modify mongo credentials to use env & move code to seperate mongo.js file

--- server/server.js
+++ server/server.js
@@ -5,83 +5,103 @@ const app = express();
 const { login, register } = require('./api/account');
 const { authenticateToken, refreshToken } = require('./utils/authentication');
 
-const fs = require('fs');
-
-let config = JSON.parse(fs.readFileSync('config.json'));
-
-const { MongoClient } = require('mongodb');
-const uri = config.uri.replace("<user>", config.username).replace("<password>", config.password)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongo = require('./utils/mongo')
 
 app.use(express.json());
 
-async function connectAndReturnUser(displayName) {
-	try {
-		await client.connect();
-		const database = client.db('HubEmpireDB');
-		const users = database.collection('Users');
-		const query = { displayName: displayName };
-
-		return await users.findOne(query);
-		} finally {
-		// Ensures that the client will close when you finish/error
-		//await client.close();
-	}
-}
-
-async function createNewUser(newUser) {
-	try {
+// async function createNewUser(newUser) {
+// 	try {
 		
-		await client.connect();
-		const database = client.db('HubEmpireDB');
-		database.collection('Users').insertOne(newUser), function(err, res){
-			if (err) throw err;
-			return 1;
-		}
-		return 0;
-		} finally {
-		// Ensures that the client will close when you finish/error
-		//await client.close();
-	}
-	MongoClient.connect(url, function(err, db) {
-		if (err) throw err;
-		var dbo = db.db("mydb");
-		var myobj = { name: "Company Inc", address: "Highway 37" };
-		dbo.collection("customers").insertOne(myobj, function(err, res) {
-		  if (err) throw err;
-		  console.log("1 document inserted");
-		  db.close();
-		});
-	  });
-}
-
+// 		await client.connect();
+// 		const database = client.db('HubEmpireDB');
+// 		database.collection('Users').insertOne(newUser), function(err, res){
+// 			if (err) throw err;
+// 			return 1;
+// 		}
+// 		return 0;
+// 		} finally {
+// 		// Ensures that the client will close when you finish/error
+// 		//await client.close();
+// 	}
+// 	MongoClient.connect(url, function(err, db) {
+// 		if (err) throw err;
+// 		var dbo = db.db("mydb");
+// 		var myobj = { name: "Company Inc", address: "Highway 37" };
+// 		dbo.collection("customers").insertOne(myobj, function(err, res) {
+// 		  if (err) throw err;
+// 		  console.log("1 document inserted");
+// 		  db.close();
+// 		});
+// 	  });
+// }
 app.get('/', function(req, res) {
-	const myPromise = 
-	(async () => {
-		res.json(await connectAndReturnUser("John").catch(console.dir));
-		console.log("async done")
-	})()
-    
+	res.redirect('/home');
 });
 
-app.get('/user/create/:displayName/', function(req, res){
-	const newUser = {
-		id: 1,
-		displayName: req.params.displayName,
-		cardIds: [],
-		cash: 0,
-		netWorth: 0,
-		netEarnings: 0,
-		assetValue: 0,
-		income: 0,
-		expenses: 0
-	};
-	(async () => {
-		await createNewUser(newUser).catch(console.dir);
-	res.json(await connectAndReturnUser(newUser.displayName).catch(console.dir));
-	})()
+app.get('/home', function(req, res) {
+	try {
+		const database = mongo.client.db('HubEmpireDB');
+		const users = database.collection('Users');
+		const query = { displayName: 'John' };
+
+
+		(async () => {
+			res.json(await users.findOne(query).catch(console.dir));
+		})()
+	} catch(error) {
+		console.error(error);
+ 	}
+});
+
+app.get('/my-cards', function(req, res) {
+	res.send('my-cards');
+});
+
+app.get('/leaderboard', function(req, res) {
+	res.send('leaderboard');
+});
+
+app.get('/users-min', function(req, res) {
+	res.send('users-min');
+});
+
+app.get('/user', function(req, res) {
+	res.send('users-min');
+});
+
+app.post('/send-trade', function(req, res) {
+	res.send('users-min');
+});
+
+app.get('/trade/inbox', function(req, res) {
+	res.send('trade/inbox');
+});
+
+app.get('/trade/history', function(req, res) {
+	res.send('trade/history');
+});
+
+app.post('/trade/action', function(req, res) {
+	res.send('trade/history');
+});
+// app.get('/user/create/:displayName/', function(req, res){
+// 	const newUser = {
+// 		id: 1,
+// 		displayName: req.params.displayName,
+// 		cardIds: [],
+// 		cash: 0,
+// 		netWorth: 0,
+// 		netEarnings: 0,
+// 		assetValue: 0,
+// 		income: 0,
+// 		expenses: 0
+// 	};
+// 	(async () => {
+// 		await createNewUser(newUser).catch(console.dir);
+// 	res.json(await connectAndReturnUser(newUser.displayName).catch(console.dir));
+// 	})()
 	
-})
+// })
 
 //Cors
 app.use((req, res, next) => {
---
+++ server/utils/mongo.js
@@ -0,0 +1,25 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+const { MongoClient } = require('mongodb');
+
+class MongoBot {
+  constructor() {
+    this.uri = process.env.MONGO_URI.replace("<user>", process.env.MONGO_USERNAME).replace("<password>", process.env.MONGO_PASSWORD)
+    this.client = new MongoClient(this.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.connect();
+  }
+
+  async connect() {
+    try{
+      await this.client.connect();
+      console.log('connected');
+    }
+    catch (error) {
+      console.error(error)
+    }
+    return;
+  }
+}
+
+module.exports = new MongoBot();
