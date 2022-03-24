d8d847d3b57baaad040199ef84163c5ab5e8abd6
ryanntannn <tanryan777@gmail.com>
2022-03-24 21:08:00 +0800

    add admin types

---
+++ client/src/types/admin.ts
@@ -0,0 +1,19 @@
+import { CardRarity } from './types';
+
+export type RewardTemplate = Map<CardRarity, number>;
+
+export interface Metric {
+	id: string;
+	displayName: string;
+	maxscore: number;
+	scoreBasedRewards: Map<number, RewardTemplate>;
+	positionBasedRewards: Map<number, RewardTemplate>;
+}
+
+export interface TestResults {
+	metricId: string;
+	results: {
+		playerId: string;
+		result: number;
+	};
+}
