4a5d43a9ead181c4ddd7105e96a295231ebc92c2
ryanntannn <tanryan777@gmail.com>
2022-03-29 13:00:15 +0800

    add buttons to admin home page

--- client/src/components/admin/AdminHome.tsx
+++ client/src/components/admin/AdminHome.tsx
@@ -1,7 +1,8 @@
-import { Alert, Container } from 'reactstrap';
+import { Alert, Col, Container, Row } from 'reactstrap';
 import Loading from '../Loading';
 import useAuth from '../../contexts/AuthenticationContext';
 import React from 'react';
+import NavButton from '../NavButton';
 
 export default function AdminHome() {
 	const auth = useAuth();
@@ -27,7 +28,54 @@ export default function AdminHome() {
 			<Container className='mt-5'>
 				<h1 className='title'>📔 Admin Page</h1>
 				{isAdmin ? (
-					<div></div>
+					<div>
+						<Row className='mb-3 gx-3'>
+							<Col>
+								<NavButton
+									title='History'
+									emoji='📓'
+									linkTo='/history'
+									color='#dd5050'
+								/>
+							</Col>
+							<Col>
+								<NavButton
+									title='Leaderboard'
+									emoji='🏆'
+									linkTo='/leaderboard'
+									color='#28A745'
+								/>
+							</Col>
+						</Row>
+						<Row className='mb-3 gx-3'>
+							<Col>
+								<NavButton
+									title='Edit Scoring Metrics'
+									emoji='⚙️'
+									linkTo='/admin/metrics'
+									color='#3333DD'
+								/>
+							</Col>
+							<Col>
+								<NavButton
+									title='Enter Test Results'
+									emoji='🖊️'
+									linkTo='/admin/test-results'
+									color='#DDA330'
+								/>
+							</Col>
+						</Row>
+						<Row className='mb-3 gx-3'>
+							<Col>
+								<NavButton
+									title='Edit Cards'
+									emoji='⚙️'
+									linkTo='/admin/edit-cards'
+									color='#DD33DD'
+								/>
+							</Col>
+						</Row>
+					</div>
 				) : isAdmin == null ? (
 					<Loading />
 				) : (
