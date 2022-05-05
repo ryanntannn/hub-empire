73277094cc4b7631048b5a26f11428b52855d055
ryanntannn <tanryan777@gmail.com>
2022-05-05 22:25:02 +0800

    change frontend for account editor

--- client/src/components/admin/AccountEditor.tsx
+++ client/src/components/admin/AccountEditor.tsx
@@ -1,5 +1,13 @@
 import React, { MutableRefObject, useRef } from 'react';
-import { Button, Container } from 'reactstrap';
+import {
+	Button,
+	Container,
+	Input,
+	Modal,
+	ModalBody,
+	ModalFooter,
+	ModalHeader,
+} from 'reactstrap';
 import BackButton from '../BackButton';
 import useAuth from '../../contexts/AuthenticationContext';
 import { Card } from '../../types/types';
@@ -7,13 +15,56 @@ import Loading from '../Loading';
 import { DropdownSelection } from '../DropdownSelection';
 import JSONEditor from 'jsoneditor';
 import 'jsoneditor/dist/jsoneditor.min.css';
+import { create, InstanceProps } from 'react-modal-promise';
+
+interface ChangePasswordProps extends InstanceProps<null, null> {
+	activeAccount: any;
+}
+
+function ChangePasswordModal(props: ChangePasswordProps) {
+	const auth = useAuth();
+	const [password, setPassword] = React.useState<string>();
+
+	async function changePassword() {
+		try {
+			auth.authenticatedPost(`/admin/changepassword`, {
+				data: { id: props.activeAccount._id, password },
+			})
+				.then((res: any) => {
+					if (res.status == 200) props.onResolve();
+				})
+				.catch((err) => console.error(err));
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	return (
+		<Modal isOpen={props.isOpen}>
+			<ModalHeader toggle={() => props.onReject()}>
+				Change Password for {props.activeAccount.profile.displayName}
+			</ModalHeader>
+			<ModalBody>
+				New Password:
+				<Input
+					type='password'
+					value={password}
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}></Input>
+			</ModalBody>
+			<ModalFooter>
+				<Button onClick={changePassword}>Submit</Button>
+			</ModalFooter>
+		</Modal>
+	);
+}
 
 export default function AccountEditor() {
 	const auth = useAuth();
 	const [accounts, setAccounts] = React.useState<any[] | null>(null);
 	const [activeAccount, setActiveAccount] = React.useState<any | null>(null);
-	let [editor, setEditor] = React.useState<JSONEditor | null>(null);
-
+	const changePasswordModal = create(ChangePasswordModal);
 	const getAccountData = () => {
 		auth.authenticatedGet(`/admin/accountdata`)
 			.then((res: any) => {
@@ -25,18 +76,9 @@ export default function AccountEditor() {
 			.catch((err) => {});
 	};
 
-	const initJsonEditor = () => {
-		setEditor(new JSONEditor(document.getElementById('jsoneditor')!, {}));
-	};
-
-	function setJson(newJson: any) {
-		editor!.set(newJson);
-	}
-
 	function setAccount(accountData: any) {
 		console.log(accountData);
-		setActiveAccount(accountData);
-		setJson(accountData);
+		setActiveAccount(JSON.parse(JSON.stringify(accountData)));
 	}
 
 	function submitAccount(accountData: any) {
@@ -51,10 +93,26 @@ export default function AccountEditor() {
 			.catch((err) => console.error(err));
 	}
 
+	function removeAccount(accountData: any) {
+		auth.authenticatedPost(`/admin/remove`, {
+			data: { ...accountData },
+		})
+			.then((res) => {
+				console.log('Account Removed!');
+				getAccountData();
+			})
+			.catch((err) => console.error(err));
+	}
+
+	async function changePassword(accountData: any) {
+		try {
+			changePasswordModal({ activeAccount: accountData });
+		} catch (err) {}
+	}
+
 	React.useEffect(() => {
 		if (accounts != null) return;
 		getAccountData();
-		initJsonEditor();
 	}, [accounts]);
 
 	return (
@@ -75,14 +133,47 @@ export default function AccountEditor() {
 				) : (
 					<Loading />
 				)}
-				<div
-					className='my-2'
-					id='jsoneditor'
-					style={{ width: '100%', height: 700 }}></div>
-
+				{activeAccount ? (
+					<>
+						Display Name:
+						<Input
+							value={activeAccount.profile.displayName}
+							onChange={(e) => {
+								setActiveAccount(({ ...prevState }) => {
+									prevState.profile.displayName =
+										e.target.value;
+									return prevState;
+								});
+							}}
+						/>
+						User Name:
+						<Input
+							value={activeAccount.profile.username}
+							onChange={(e) => {
+								setActiveAccount(({ ...prevState }) => {
+									prevState.profile.username = e.target.value;
+									return prevState;
+								});
+							}}
+						/>
+						<br />
+						<Button
+							className='me-2'
+							color='primary'
+							onClick={() => changePassword(activeAccount)}>
+							Change Password
+						</Button>
+						<Button
+							className='me-2'
+							color='danger'
+							onClick={() => removeAccount(activeAccount)}>
+							Remove Account
+						</Button>
+					</>
+				) : null}
 				<Button
 					color='success'
-					onClick={() => submitAccount(editor!.get())}>
+					onClick={() => submitAccount(activeAccount)}>
 					Submit
 				</Button>
 			</Container>
